e45d10fca89c9611300dc3d90df7061a1a872aac
Lauren <kerk0100@mylaurier.ca>
2021-01-16 16:28:36 -0500

    pause

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,17 +1,17 @@
-// function playAudio(url) {
-//     new Audio(url).play();
-// }
-
-// function pauseAudio(url) {
-//     new Audio(url).pause();
-// }
-
 function playAudio(url) {
-    var audio = new Audio(url);
-    if (audio.paused) {
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-    }
+    new Audio(url).play();
 }
+
+function pauseAudio(url) {
+    new Audio(url).pause();
+}
+
+// function playAudio(url) {
+//     var audio = new Audio(url);
+//     if (audio.paused) {
+//         audio.play();
+//     }else{
+//         audio.pause();
+//         audio.currentTime = 0
+//     }
+// }
