e2e6ff780c424d50e539a1deb72619a2f04d9982
Lauren <kerk0100@mylaurier.ca>
2021-01-16 15:37:51 -0500

    audio

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,7 +1,16 @@
-function playAudio(url) {
-    new Audio(url).play();
-  }
+// function playAudio(url) {
+//     new Audio(url).play();
+//   }
 
-  function pauseAudio(url) {
-    new Audio(url).pause();
-  }
+// function pauseAudio(url) {
+//     new Audio(url).pause();
+//   }
+
+function playAudio(url) {
+    if (Audio(url).paused) {
+        Audio(url).play();
+    }else{
+        Audio(url).pause();
+        Audio(url).currentTime = 0
+    }
+}
